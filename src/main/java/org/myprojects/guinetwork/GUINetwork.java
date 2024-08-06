4b7d7224e4753c2e9b70435958d94ab9c4a118e9
Davie Munthali <davemunthali@gmail.com>
2024-08-06 10:55:24 +0200

    first and last

---
+++ src/main/java/org/myprojects/guinetwork/GUINetwork.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.myprojects.guinetwork;
+
+/**
+ *
+ * @author Admin
+ */
+public class GUINetwork {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/org/myprojects/guinetwork/models/Post.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.myprojects.guinetwork.models;
+
+import com.google.gson.annotations.Expose;
+
+/**
+ *
+ * @author Admin
+ */
+public class Post {
+    @Expose
+    private int userId;
+    @Expose
+    private int id;
+    @Expose
+    private String title;
+    @Expose
+    private String body;
+
+    public Post() {
+    }
+
+    public Post(int userId, int id, String title, String body) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+   
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getPostId() {
+        return id;
+    }
+
+    public void setPostId(int postId) {
+        this.id = postId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String description) {
+        this.body = description;
+    }
+    
+    
+}
